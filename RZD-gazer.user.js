e1419af73d110e251f94ad3b2d5c8a874d0d6ae1
Dmitry Vyal <akamaus@gmail.com>
2014-03-07 11:38:29 +0400

    адаптирована под ожидание билетов на экспресс

--- RZD-gazer.user.js
+++ RZD-gazer.user.js
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 $(document).ready(function() {
-  
+
   setTimeout(make_test, 10000);
 
   function make_test() {
@@ -18,14 +18,14 @@ $(document).ready(function() {
       $('input.stdButton').click();
       return;
     }
-    
+
     if ($('table.LogonTableForm').length) { // логинимся (2ой вариант экрана)
       console.log('logging in');
       $('input#other').click();
     }
-    
+
     console.log("len " + $('div.errorMessageDiv').length);
-    if (($("div.dateNavig").length == 0) || ($('div.errorMessageDiv').length >= 1)) {
+    if (!detect_presense() || ($('div.errorMessageDiv').length >= 1)) {
       console.log("issuing reload");
       setTimeout(function() {
 	window.location.reload();
@@ -40,5 +40,31 @@ $(document).ready(function() {
     var snd = new Audio("http://www.lunerouge.org/sons/engins/LRObject%20falling%2001%20by%20Lionel%20Allorge.ogg");
     snd.play();
   };
+
+  function detect_presense() {
+      var target_trains = ["№ 7044"];
+
+      var issue_alarm = false;
+
+      console.log("Hello");
+      $(".trlist__trlist-row").each(function() {
+          var train = $("div.train-num-1", this).text();
+          if (train=="") {
+              train = $("div.train-num-0", this).text();
+          }
+          if (train=="") return;
+
+//          console.log(train);
+
+          if ($.inArray(train, target_trains) > -1) {
+              console.log("train " + train + " detected");
+              if ( $("table.trlist__table-price", this).length > 0) {
+                  console.log("tickets for train " + train + " available");
+                  issue_alarm = true;
+              }
+          }
+      });
+      return issue_alarm;
+  }
 
 });
