2041a1a3449fba0f9b0893244d257d9eeb637d16
Dmitry Vyal <akamaus@gmail.com>
2014-03-16 14:31:40 +0400

    поиск нужного вагона в поезде

--- RZD-gazer.user.js
+++ RZD-gazer.user.js
@@ -9,11 +9,12 @@
 // ==/UserScript==
 
 $(document).ready(function() {
+  console.log("RZD-gazer is active");
 
   setTimeout(make_test, 10000);
 
   function make_test() {
-    if ($('input.stdButton').length) { // логинимся (1ый вариант экрана
+      if ($('input.stdButton').length) { // логинимся (1ый вариант экрана
       console.log('logging in');
       $('input.stdButton').click();
       return;
@@ -24,47 +25,81 @@ $(document).ready(function() {
       $('input#other').click();
     }
 
-    console.log("len " + $('div.errorMessageDiv').length);
-    if (!detect_presense() || ($('div.errorMessageDiv').length >= 1)) {
-      console.log("issuing reload");
-      setTimeout(function() {
-	window.location.reload();
-      }, 10000);
-    } else {
-      console.log("issuing alarm");
-      setInterval(alarm, 10000);
+    if ($('div.errorMessageDiv').length >= 1) {
+        issue_reload();
+    }
+
+    var section = $.trim($('div#order_ticket_path div.current').text());
+    console.log("section = " + section);
+
+    switch(section) {
+    case 'Выборпоезда':
+        test_train();
+        break;
+    case 'Выборвагона':
+        test_wagon();
+        break;
+    default:
+        console.log('WARNING: unknown screen, do nothing');
     }
   }
 
-  function alarm() {
+  function test_train() {
+    var target_trains = ["№ 7044"];
+
+    var train_found = false;
+
+    console.log("Hello");
+    $(".trlist__trlist-row").each(function() {
+      var train = $("div.train-num-1", this).text();
+      if (train=="") {
+          train = $("div.train-num-0", this).text();
+      }
+      if (train=="") return;
+
+      if ($.inArray(train, target_trains) > -1) {
+          console.log("train " + train + " detected");
+          if ( $("table.trlist__table-price", this).length > 0) {
+              console.log("tickets for train " + train + " available");
+              train_found = true;
+          }
+      }
+    });
+
+    if(train_found)
+        issue_alarm();
+    else issue_reload();
+  }
+
+  function test_wagon() {
+    var target_wagons = [ "06", "07", "08"];
+    var wagon_found = false;
+
+    $('table.pass_cars_table label:has(input)').each(function() {
+      var wagon = $.trim($(this).text());
+      var pos = $.inArray(wagon, target_wagons);
+      if ( pos > -1) {
+          console.log("Tickets for wagon " + target_wagons[pos] + " available");
+          wagon_found = true;
+      }
+    });
+    if (wagon_found)
+        issue_alarm();
+    else issue_reload();
+  }
+
+  function issue_alarm() {
+    console.log("issuing alarm");
+    setInterval(do_alarm, 10000);
+  }
+
+  function do_alarm() {
     var snd = new Audio("http://www.lunerouge.org/sons/engins/LRObject%20falling%2001%20by%20Lionel%20Allorge.ogg");
     snd.play();
   };
 
-  function detect_presense() {
-      var target_trains = ["№ 7044"];
-
-      var issue_alarm = false;
-
-      console.log("Hello");
-      $(".trlist__trlist-row").each(function() {
-          var train = $("div.train-num-1", this).text();
-          if (train=="") {
-              train = $("div.train-num-0", this).text();
-          }
-          if (train=="") return;
-
-//          console.log(train);
-
-          if ($.inArray(train, target_trains) > -1) {
-              console.log("train " + train + " detected");
-              if ( $("table.trlist__table-price", this).length > 0) {
-                  console.log("tickets for train " + train + " available");
-                  issue_alarm = true;
-              }
-          }
-      });
-      return issue_alarm;
+  function issue_reload() {
+    console.log("issuing reload");
+    setTimeout(function() { window.location.reload(); }, 10000);
   }
-
 });
